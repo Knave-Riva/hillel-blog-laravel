135853fac8c7ec77eb55e0284f3b2af54199dd34
Knave <knaveriva@gmail.com>
2019-09-05 15:20:29 +0300

    Add Posts table commit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -99,72 +99,23 @@
                         <!-- Nav Start -->
                         <div class="classynav">
                             <ul>
-                                <li><a href="index.html">Home</a></li>
-                                <li><a href="#">Pages</a>
+                                <li><a href="{{route('blog-index')}}">Home</a></li>
+                                <li><a href="{{route('blog-blog')}}">Blog</a>
                                     <ul class="dropdown">
-                                        <li><a href="index.html">Home</a></li>
-                                        <li><a href="archive-blog.html">Archive Blog</a></li>
-                                        <li><a href="single-post.html">Single Post</a></li>
-                                        <li><a href="about-us.html">About</a></li>
-                                        <li><a href="contact.html">Contact</a></li>
-                                        <li><a href="typography.html">Typography</a></li>
+                                        <li><a href="#">Features</a></li>
+                                        <li><a href="#">Food</a></li>
+                                        <li><a href="#">Travel</a></li>
+                                        <li><a href="#">Recipe</a></li>
+                                        <li><a href="#">Bread</a></li>
+                                        <li><a href="#">Breakfast</a></li>
+                                        <li><a href="#">Meat</a></li>
+                                        <li><a href="#">Fastfood</a></li>
+                                        <li><a href="#">Salad</a></li>
+                                        <li><a href="#">Soup</a></li>
                                     </ul>
                                 </li>
-                                <li><a href="#">Catagories</a>
-                                    <div class="megamenu">
-                                        <ul class="single-mega cn-col-4">
-                                            <li><a href="#">- Features</a></li>
-                                            <li><a href="#">- Food</a></li>
-                                            <li><a href="#">- Travel</a></li>
-                                            <li><a href="#">- Recipe</a></li>
-                                            <li><a href="#">- Bread</a></li>
-                                            <li><a href="#">- Breakfast</a></li>
-                                            <li><a href="#">- Meat</a></li>
-                                            <li><a href="#">- Fastfood</a></li>
-                                            <li><a href="#">- Salad</a></li>
-                                            <li><a href="#">- Soup</a></li>
-                                        </ul>
-                                        <ul class="single-mega cn-col-4">
-                                            <li><a href="#">- Features</a></li>
-                                            <li><a href="#">- Food</a></li>
-                                            <li><a href="#">- Travel</a></li>
-                                            <li><a href="#">- Recipe</a></li>
-                                            <li><a href="#">- Bread</a></li>
-                                            <li><a href="#">- Breakfast</a></li>
-                                            <li><a href="#">- Meat</a></li>
-                                            <li><a href="#">- Fastfood</a></li>
-                                            <li><a href="#">- Salad</a></li>
-                                            <li><a href="#">- Soup</a></li>
-                                        </ul>
-                                        <ul class="single-mega cn-col-4">
-                                            <li><a href="#">- Features</a></li>
-                                            <li><a href="#">- Food</a></li>
-                                            <li><a href="#">- Travel</a></li>
-                                            <li><a href="#">- Recipe</a></li>
-                                            <li><a href="#">- Bread</a></li>
-                                            <li><a href="#">- Breakfast</a></li>
-                                            <li><a href="#">- Meat</a></li>
-                                            <li><a href="#">- Fastfood</a></li>
-                                            <li><a href="#">- Salad</a></li>
-                                            <li><a href="#">- Soup</a></li>
-                                        </ul>
-                                        <ul class="single-mega cn-col-4">
-                                            <li><a href="#">- Features</a></li>
-                                            <li><a href="#">- Food</a></li>
-                                            <li><a href="#">- Travel</a></li>
-                                            <li><a href="#">- Recipe</a></li>
-                                            <li><a href="#">- Bread</a></li>
-                                            <li><a href="#">- Breakfast</a></li>
-                                            <li><a href="#">- Meat</a></li>
-                                            <li><a href="#">- Fastfood</a></li>
-                                            <li><a href="#">- Salad</a></li>
-                                            <li><a href="#">- Soup</a></li>
-                                        </ul>
-                                    </div>
-                                </li>
-                                <li><a href="#">Travel</a></li>
-                                <li><a href="about-us.html">About</a></li>
-                                <li><a href="contact.html">Contact</a></li>
+                                <li><a href="{{route('blog-about')}}">About</a></li>
+                                <li><a href="{{route('blog-contact')}}">Contact</a></li>
                             </ul>
 
                         </div>
--- routes/web.php
+++ routes/web.php
@@ -4,24 +4,21 @@
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('blog-index');
 
 Route::get('/about-us', function () {
     return view('about_us');
-});
+})->name('blog-about');
 
 Route::get('/contacts', function () {
     return view('contact');
-});
+})->name('blog-contact');
 
 Route::get('/blog', function () {
     return view('blog');
-});
+})->name('blog-blog');
 
 Route::get('/posts', function () {
     return view('post');
-});
+})->name('blog-post');
 
-Route::get('/typography', function () {
-    return view('typography');
-});
