53c9d36a24ea49fc7a5175480b7405a69316355e
Knave <knaveriva@gmail.com>
2019-09-05 17:31:31 +0300

    Add status table commit

--- database/migrations/2019_09_05_141731_add_status_to_posts_table.php
+++ database/migrations/2019_09_05_141731_add_status_to_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class AddStatusToPostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,22 +13,20 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->char('title');
-            $table->string('description',100);
-            $table->text('body');
-            $table->timestamps();
+        Schema::table('posts', function (Blueprint $table) {
+            $table->boolean('status')->after('body')->default(0);
         });
     }
 
-    /**text
+    /**
      * Reverse the migrations.
      *
      * @return void
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
     }
 }
--- database/migrations/2019_09_05_142233_add_changes_title_and_description_to_posts_table.php
+++ database/migrations/2019_09_05_142233_add_changes_title_and_description_to_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class AddChangesTitleAndDescriptionToPostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,22 +13,21 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->char('title');
-            $table->string('description',100);
-            $table->text('body');
-            $table->timestamps();
+        Schema::table('posts', function (Blueprint $table) {
+            $table->char('title')->string();
+            $table->varchar('description',100)->varchar()->nullable();
         });
     }
 
-    /**text
+    /**
      * Reverse the migrations.
      *
      * @return void
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
     }
 }
