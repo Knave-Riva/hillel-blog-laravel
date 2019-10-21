f344358f50edfa06aa1d1fe434534e264c051c2a
Knave <knaveriva@gmail.com>
2019-10-21 16:07:26 +0300

    Создание админки категории доработка

--- resources/views/admin/categories/category-update.blade.php
+++ resources/views/admin/categories/category-update.blade.php
@@ -9,23 +9,36 @@
 @stop
 
 @section('content')
+    <div class="row">
+        <!-- left column -->
+        <div class="col-md-3">
 
     <form method="POST" action="{{ route('categories.update', $category->id) }}">
         @method('PUT')
+
         <div class="form-group">
-            <input type="text" name="title" @if($errors->has('title')) style="border-color: red" @endif class="error" value="{{@old('title', $category->title)}}">
-            <label for="exampleInputEmail1">Category</label>
-        </div>
+            @if($errors->has('title'))
+                    <input type="text" name="title" class="form-control is-warning" id="inputWarning" value="{{@old('title', $category->title)}}">
+                    <label class="col-form-label" for="inputWarning"><i class="far fa-bell"></i> Input with
+                        warning</label>
+            @else  <input type="text" name="title" class="form-control is-valid"  id="inputSuccess" value="{{@old('title', $category->title)}}">
+                    <label class="col-form-label" for="inputSuccess"><i class="fas fa-check"></i> Input with
+                        success</label>
+            @endif
         @if($errors->has('title'))
             @foreach($errors->get('title') as $error)
                 <p>{{$error}}</p>
             @endforeach
         @endif
         <br>
-        <div class="form-group">
-            <input type="text" name="slug" @if($errors->has('slug')) style="border-color: red" @endif class="error" value="{{@old('title', $category->slug)}}">
-            <label for="exampleInputPassword1">Slug</label>
-        </div>
+             @if($errors->has('slug'))
+                   <input type="text" name="slug" class="form-control is-warning" id="inputWarning" value="{{ @old('slug', $category->slug) }}">
+                   <label class="col-form-label" for="inputWarning"><i class="far fa-bell"></i> Input with
+                   warning</label>
+             @else  <input type="text" name="slug" class="form-control is-valid"  id="inputSuccess" value="{{ @old('slug', $category->slug) }}">
+                    <label class="col-form-label" for="inputSuccess"><i class="fas fa-check"></i> Input with
+                    success</label>
+             @endif
         <br>
         @if($errors->has('slug'))
             @foreach($errors->get('slug') as $error)
@@ -33,9 +46,11 @@
             @endforeach
         @endif
         <button type="submit" value="Update">Update</button>
-
+        </div>
         @csrf
     </form>
+    </div>
+    </div>
 
 @stop
 
@@ -48,3 +63,38 @@
 @stop
 
 
+{{--<form role="form" method="POST" action="{{ route('categories.store') }}">--}}
+{{--    <div class="form-group">--}}
+{{--        @if($errors->has('title'))--}}
+{{--            <input type="text" name="title" class="form-control is-warning" id="inputWarning" value="{{ @old('title') }}">--}}
+{{--            <label class="col-form-label" for="inputWarning"><i class="far fa-bell"></i> Input with--}}
+{{--                warning</label>--}}
+{{--        @else  <input type="text" name="title" class="form-control is-valid"  id="inputSuccess" value="{{ @old('title') }}">--}}
+{{--        <label class="col-form-label" for="inputSuccess"><i class="fas fa-check"></i> Input with--}}
+{{--            success</label>--}}
+{{--        @endif--}}
+{{--    </div>--}}
+{{--    @if($errors->has('title'))--}}
+{{--        @foreach($errors->get('title') as $error)--}}
+{{--            <p>{{$error}}</p>--}}
+{{--        @endforeach--}}
+{{--    @endif--}}
+{{--    <br>--}}
+{{--    @if($errors->has('slug'))--}}
+{{--        <input type="text" name="slug" class="form-control is-warning" id="inputWarning" value="{{ @old('slug') }}">--}}
+{{--        <label class="col-form-label" for="inputWarning"><i class="far fa-bell"></i> Input with--}}
+{{--            warning</label>--}}
+{{--    @else  <input type="text" name="slug" class="form-control is-valid"  id="inputSuccess" value="{{ @old('slug') }}">--}}
+{{--    <label class="col-form-label" for="inputSuccess"><i class="fas fa-check"></i> Input with--}}
+{{--        success</label>--}}
+{{--    @endif--}}
+{{--    <br>--}}
+{{--    @if($errors->has('slug'))--}}
+{{--        @foreach($errors->get('slug') as $error)--}}
+{{--            <p>{{$error}}</p>--}}
+{{--        @endforeach--}}
+{{--    @endif--}}
+{{--    <button type="submit" value="Create">Create</button>--}}
+
+{{--    @csrf--}}
+{{--</form>--}}
